6fa6441bee512954bea8801355ca2990a5c02f58
Your Name <you@example.com>
2017-05-11 19:41:49 +0900

    2017-05-11  genhwi test

--- workSpace/GitTest/src/TestClass.java
+++ workSpace/GitTest/src/TestClass.java
@@ -9,5 +9,7 @@ public class TestClass {
 		
 		System.out.println("그 다음 테스트사람 이 글 지우고 이름적어주세요");
 		System.out.println("TEST*********************************");
+		System.out.println("yang");
+		System.out.println("TEST*********************************");
 	}
 }
